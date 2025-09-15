17497f9753748fc80139601b1893a233cdf00a71
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-16 01:48:50 +0300

    Koşullu durum

--- src/App.jsx
+++ src/App.jsx
@@ -29,42 +29,55 @@ function MoviewList() {
       image: "1.jpg",
       baslik: "Kaptan Amerika",
       aciklama: "Güzel bir film",
+      is_active: true,
     },
     {
       image: "2.jpg",
       baslik: "Araba Hırsızları",
       aciklama: "Güzel bir film",
+      is_active: true,
     },
     {
       image: "3.jpg",
       baslik: "Araba Hırsızları",
       aciklama: "Güzel bir film",
+      is_active: false,
     },
     {
       image: "4.jpg",
       baslik: "Araba Hırsızları",
       aciklama: "Güzel bir film",
+      is_active: true,
     },
   ];
 
   return (
     <div>
       <h1>Moview List</h1>
-      <div id="movie-list">
-        {movie_list.map((movie, index) => (
-          <Movie key={index} movieObj={movie} />
-        ))}
-      </div>
+
+      {movie_list.filter((m) => m.is_active).length === 0 ? (
+        <div>Listede film bulunmamaktadır.</div>
+      ) : (
+        <div id="movie-list">
+          {movie_list.map((movie, index) => (
+            <Movie key={index} movieObj={movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 function Movie({ movieObj }) {
   return (
-    <div className="movie">
-      <img src={"/img/" + movieObj.image} alt="" />
-      <h3>{movieObj.baslik}</h3>
-      <p>{movieObj.aciklama}</p>
+    <div>
+      {movieObj.is_active && (
+        <div className="movie">
+          <img src={"/img/" + movieObj.image} alt="" />
+          <h3>{movieObj.baslik}</h3>
+          <p>{movieObj.aciklama}</p>
+        </div>
+      )}
     </div>
   );
 }
